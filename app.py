24a4d7c92cfc351149661c7244666b3948b73a80
tx-smitht <tx.smiththomas@gmail.com>
2023-04-06 18:29:39 -0600

    added model and prediction functionality

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import pickle
+import pandas as pd
+from sklearn import preprocessing
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -16,3 +19,16 @@ app.add_middleware(
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to the API!"}
+
+# Define endpoint for making predictions
+@app.post('/predict')
+def predict(data:dict):
+  # Load model from .pkl file
+  with open('./model.pkl','rb') as file:
+    model = pickle.load(file)
+    # Convert input data to DataFrame
+    df = pd.DataFrame(data, index=[0])
+    # Make prediction
+    prediction = model.predict(df)
+    # Return Prediction as JSON response
+    return {'prediction': prediction[0]}
